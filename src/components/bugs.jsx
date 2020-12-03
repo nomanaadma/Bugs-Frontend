a6f1e1fe74356120a7fb48a5a173f02509c12320
Noman Shoukat <nomanaadma@gmail.com>
2020-12-03 15:44:48 +0500

    Dispatching Resolve Bug Action

--- src/components/bugs.jsx
+++ src/components/bugs.jsx
@@ -1,25 +1,33 @@
 import { Component } from "react";
-import { loadbugs } from '../store/bugs';
-import { connect } from 'react-redux';
+import { getUnresolvedBugs, loadbugs, resolveBug } from "../store/bugs";
+import { connect } from "react-redux";
 
 class Bugs extends Component {
-
 	componentDidMount() {
 		this.props.loadBugs();
 	}
 
 	render() {
-		return <ul>{this.props.bugs.map(bug => <li key={bug.id}>{bug.description}</li>)}</ul>
+		return (
+			<ul>
+				{this.props.bugs.map(bug => (
+					<li key={bug.id}>
+						{bug.description} 
+						<button onClick={() => this.props.resolveBug(bug.id)}>Resolve</button>
+					</li>
+				))}
+			</ul>
+		);
 	}
-
 }
 
 const mapStateToProps = state => ({
-	bugs: state.entities.bugs.list
+	bugs: getUnresolvedBugs(state),
 });
 
 const mapDispatchToProps = dispatch => ({
-	loadBugs: () => dispatch(loadbugs())
+	loadBugs: () => dispatch(loadbugs()),
+	resolveBug: id => dispatch(resolveBug(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Bugs);
--- src/components/bugsLists.jsx
+++ src/components/bugsLists.jsx
@@ -1,9 +1,8 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loadbugs, getUnresolvedBugs } from '../store/bugs';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadbugs, getUnresolvedBugs, resolveBug } from "../store/bugs";
 
-const bugsLists = () => {
-
+const BugsLists = () => {
 	const dispatch = useDispatch();
 	const bugs = useSelector(getUnresolvedBugs);
 
@@ -11,7 +10,16 @@ const bugsLists = () => {
 		dispatch(loadbugs());
 	}, []);
 
-	return <ul>{bugs.map(bug => <li key={bug.id}>{bug.description}</li>)}</ul>
+	return (
+		<ul>
+			{bugs.map(bug => (
+				<li key={bug.id}>
+					{bug.description}
+					<button onClick={() => dispatch(resolveBug(bug.id))}>Resolve</button>
+				</li>
+			))}
+		</ul>
+	);
 };
 
-export default bugsLists;
+export default BugsLists;
