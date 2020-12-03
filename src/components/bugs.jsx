8a34c5dd3b3768fa57c0ab0f18aca3762093a718
Noman Shoukat <nomanaadma@gmail.com>
2020-12-03 15:05:07 +0500

    Context Approach of Store

--- src/components/bugs.jsx
+++ src/components/bugs.jsx
@@ -1,12 +1,32 @@
 import { Component } from "react";
 import StoreContext from '../contexts/storeContext';
+import { loadbugs } from '../store/bugs';
 
 class Bugs extends Component {
 
 	static contextType = StoreContext;
+	state = { bugs: [] }
+
+	componentDidMount() {
+		const store = this.context;
+
+		this.unsubscribe = store.subscribe(() => {
+			const bugsInStore = store.getState().entities.bugs.list;
+			if(this.state.bugs !== bugsInStore) {
+				this.setState({ bugs: bugsInStore });
+			}
+		});
+
+		store.dispatch(loadbugs());
+
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
 
 	render() {
-		return (<div>bugs</div>)
+		return <ul>{this.state.bugs.map(bug => <li key={bug.id}>{bug.description}</li>)}</ul>
 	}
 
 }
